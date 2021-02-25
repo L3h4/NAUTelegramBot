461bca3e4e2a36ba5a9ce33be091a4ad3740f413
githubuser402 <colomborin53@gmail.com>
2021-02-25 14:29:25 +0200

    without changes

--- settings.py
+++ settings.py
@@ -3,9 +3,10 @@ import os
 load_dotenv()
 
 #       .env
-# BOT_TOKEN="1231312:ABCDEFGKADLKLakdADK"
+BOT_TOKEN="1231312:ABCDEFGKADLKLakdADK"
 #
 #
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+print(BOT_TOKEN)
 
