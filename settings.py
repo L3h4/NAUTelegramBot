39b55c9e1605396ec38ba4de21cb1967e1550b23
githubuser402 <colomborin53@gmail.com>
2021-02-25 15:08:47 +0200

    added NAUBOT

--- settings.py
+++ settings.py
@@ -3,10 +3,9 @@ import os
 load_dotenv()
 
 #       .env
-BOT_TOKEN="1231312:ABCDEFGKADLKLakdADK"
-#
+# BOT_TOKEN="1231312:ABCDEFGKADLKLakdADK"
+# NAUBOT
 #
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-print(BOT_TOKEN)
 
