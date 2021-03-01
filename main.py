68915844a93d049fb4b3efe86a61a8a1c20eaa13
githubuser402 <colomborin53@gmail.com>
2021-03-01 14:21:00 +0200

    added logger(will be modified)

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import telebot
 from telebot import types
 import settings
+from loguru import logger
 
+def Bot():
+    @logger.catch
+    @bot.message_handler(commands = ['start'])
+    def start_func(message):
+        logger.debug(f'{message.from_user.id} {message.from_user.first_name} started the bot')
+        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}, i am NAUBOT')
+    
+    @logger.catch
+    @bot.message_handler(content_types = ['text'])
+    def echo(message):
+        logger.debug(f'{message.from_user.id} {message.from_user.first_name} {message.text}')
+        res = int(message.text)
+        bot.send_message(message.chat.id, f'{res**2}')        
 
-
-bot = telebot.TeleBot(settings.BOT_TOKEN)
-
-@bot.message_handler(content_types=["text"])
-def on_text(message : types.Message):
-  bot.reply_to(message, message.text)
-
-
-
-
-bot.polling(True)
+if __name__ == '__main__':
+    bot = telebot.TeleBot(settings.BOT_TOKEN)
+    #створюється папка 
+    logger.add('logs/debug.log', format = '{time} {level} {message}', rotation = '100 KB', compression = 'zip')
+    Bot()
+    bot.polling(True)
+    
