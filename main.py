9bf41b7e136408e0c1ed2e34b9b041dca7ee7e3f
L3h4 <38466857+L3h4@users.noreply.github.com>
2021-03-11 21:48:24 +0200

    commit to push

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import telebot
-from telebot import types
 from loguru import logger
 
 import settings
-from Bot.UserManager import UserManager
-
-@logger.catch
-def Bot():
-    UM = UserManager()
-
-    @bot.message_handler(commands = ['start'])
-    @UM.Wraps
-    def start_func(message, user):
-        logger.info(f'{user} : started the bot')
-        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}, i am NAUBOT')
-    
-
-    @bot.message_handler(content_types = ['text'])
-    def echo(message):
-        logger.info(f'{message.from_user.id} {message.from_user.first_name} {message.text}')
-        res = int(message.text)
-        bot.send_message(message.chat.id, f'{res**2}')        
+from Bot import Bot 
 
 
-if __name__ == '__main__':
+def main():
     bot = telebot.TeleBot(settings.BOT_TOKEN)
     #створюється папка з логами(внесена в .gitignore)
     logger.add('logs/debug.log', format = '{time} {level} {message}', rotation = '100 KB', compression = 'zip')
-    Bot()
+    Bot(bot)
     bot.polling(True)
+
+if __name__ == '__main__':
+    main()
     
