b2f4c36b13c6185111992a9e28a7b9d9930f1d14
githubuser402 <colomborin53@gmail.com>
2021-03-31 15:41:30 +0300

    raw commit of pdf rec handler

--- Bot/handlers/PDF_receiving.py
+++ Bot/handlers/PDF_receiving.py
@@ -1,9 +1,40 @@
 from loader import bot, logger
 
+import os
+
+from pdf2image import convert_from_path
+
+@logger.catch()
+def convertPdf(pdfPath, imgMainDir, src):
+    imgPath = imgMainDir + '/' + src[0]#треба. щоб перша цифра назви файлу вказувала на курс
+    if not os.path.exists(imgPath):
+        os.mkdir(imgPath)
+    pages = convert_from_path(pdfPath + '/' + src, 200)
+    
+    for i, page in enumerate(pages):
+        path = f'{imgPath}/{i+1}.jpg'
+        page.save(path, 'JPEG')
+        logger.info(f'JPRG {path} created')
+    
+    os.remove(pdfPath + '/' + src)#видалення pdf-файлу пісял розбиття
+
 
 @bot.message_handler(content_types = ['document'])
 @logger.catch
 def file_receiving(message):
-    #use admins id
-    if message.from_user.id == '559592369':
-        bot.send_message(message.chat.id, 'Ok, file received')
+  #use admins id
+  save_dir = 'Data/stud_files/PDFs'#директорія з pdf файлами
+  if str(message.chat.id) == 'your id':#цю частину дороблю потім
+    src = message.document.file_name
+    if '.pdf' in src:#перевіркана типу фвйлу
+      file_id_info = bot.get_file(message.document.file_id)
+      downloaded_file = bot.download_file(file_id_info.file_path)
+      with open(save_dir + "/" + src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+      logger.info(f'filename is {message.document.file_name}')
+      bot.send_message(message.chat.id, 'pdf downloaded')
+
+      convertPdf(save_dir,'Data/stud_files/JPGs', src)#орзбиття документу на фото
+    else:
+      bot.send_message(message.chat.id, 'another type of file sended')
+    
--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from Bot.middleware import middleware
 
 from Bot.handlers import *
 
+from Bot.handlers import PDF_receiving
+
 
 @logger.catch
 def main():
   logger.add('logs/debug.log', format = '{time} {level} {message}', rotation = '100 KB', compression = 'zip')
   echo
+  PDF_receiving.file_receiving
   bot.polling(True)
 
 if __name__ == "__main__":
